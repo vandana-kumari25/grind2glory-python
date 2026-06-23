4d461b16c055a8642fe3e5b1b6c10ab50bb42191
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2026-06-23 23:30:45 +0530

    Solution of ransomNote using hasmap and frequency count

---
+++ grinding/ransomenote.py
@@ -0,0 +1,13 @@
+def canConstruct(self, ransomNote, magazine):
+        count = {}
+
+        for ch in magazine:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in ransomNote:
+            if count.get(ch, 0) == 0:
+                return False
+
+            count[ch] -= 1
+
+        return True
