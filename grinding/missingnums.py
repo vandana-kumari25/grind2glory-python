3d588ad6f089142d2fd1544fbefeb6e247615aa5
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2026-06-25 22:24:27 +0530

    Solution of missing number using hashset

---
+++ grinding/missingnums.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        seen = set(nums)
+        result = []
+
+        for i in range(1, len(nums) + 1):
+            if i not in seen:
+                result.append(i)
+
+        return result
+
+nums = [4,3,2,7,8,2,3,1]
+
+sol = Solution()
+
+print(sol.findDisappearedNumbers(nums))
+
+
+# return nums
+# class Solution(object):
+#    def missingNumber(self, nums):
+#        """
+#        :type nums: List[int]
+#        :rtype: int
+#        """
+#        seen = set(nums)
+#
+#        for i in range(len(nums) + 1):
+#            if i not in seen:
+#                return i
+        
